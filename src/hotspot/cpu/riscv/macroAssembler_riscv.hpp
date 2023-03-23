c3083f9a68f493ea0b214d0182c08f406ebc0fc3
zhengxiaolinX <yunyao.zxl@alibaba-inc.com>
2023-03-23 17:12:36 +0800

    8294086: RISC-V: Cleanup InstructionMark usages in the backend
    Reviewed-by: fjiang, fyang

--- src/hotspot/cpu/riscv/macroAssembler_riscv.hpp
+++ src/hotspot/cpu/riscv/macroAssembler_riscv.hpp
@@ -648,7 +648,7 @@ class MacroAssembler: public Assembler {
   void reserved_stack_check();
 
   void get_polling_page(Register dest, relocInfo::relocType rtype);
-  address read_polling_page(Register r, int32_t offset, relocInfo::relocType rtype);
+  void read_polling_page(Register r, int32_t offset, relocInfo::relocType rtype);
 
   address trampoline_call(Address entry, CodeBuffer* cbuf = NULL);
   address ic_call(address entry, jint method_index = 0);
